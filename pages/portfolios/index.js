fbbd984fb63e5dd9e0cb8574739d3a67afedbf42
salybu <rkdmsdl93@gmail.com>
2021-12-13 17:56:09 +0900

    feat: fecth data on the client with graphql

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,15 +1,30 @@
-const apiCall = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res({ testingData: "Just some testing Data" });
-    }, 5000);
-  });
+import axios from "axios";
+import PortfolioCard from "@/components/portfolios/PortfolioCard";
+
+const fetchPortfolios = () => {
+  const query = `
+  query Portfolios {
+      portfolios {
+        _id,
+        title,
+        company,
+        companyWebsite,
+        location,
+        jobTitle,
+        description
+        startDate
+        endDate
+      }
+    }`;
+  return axios
+    .post(`http://localhost:3000/graphql`, { query })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.portfolios);
 };
 
-const Portfolios = (props) => {
+const Portfolios = ({ portfolios }) => {
   return (
     <>
-      {props.testingData}
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
@@ -17,53 +32,14 @@ const Portfolios = (props) => {
           </div>
         </div>
       </section>
+      {/* {JSON.stringify(portfolios)} */}
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
+          {portfolios.map((portfolio) => (
+            <div key={portfolio._id} className="col-md-4">
+              <PortfolioCard portfolio={portfolio} />
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
@@ -71,9 +47,8 @@ const Portfolios = (props) => {
 };
 
 Portfolios.getInitialProps = async () => {
-  console.log("GET INITIAL PROPS PORTFOLIOS");
-  const data = await apiCall();
-  return { ...data };
+  const portfolios = await fetchPortfolios();
+  return { portfolios };
 };
 
 export default Portfolios;

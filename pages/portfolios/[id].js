f083a6595f76259255bd0abd9de2fc6f2595fb9e
salybu <rkdmsdl93@gmail.com>
2021-12-13 20:17:31 +0900

    feat: portfolio detail page with graphql

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,24 +1,79 @@
 import React from "react";
-import { useRouter } from "next/router";
+import axios from "axios";
 
-/* funtional component #1 */
-// const PortfolioDetail = () => {
-//   const router = useRouter();
-//   // const id = router.query.id;
-//   const { id } = router.query;
+const fetchPortfolioById = (id) => {
+  const query = `
+  query Portfolio($id: ID) {
+    portfolio(id: $id) {
+      _id,
+      title,
+      company,
+      companyWebsite,
+      location,
+      jobTitle,
+      description
+      startDate
+      endDate
+    }
+  }`;
+  const variables = { id };
 
-//   return <h1>I am Detail Page with ID: {id}</h1>;
-// };
-
-/* funtional component #2 */
-const PortfolioDetail = ({ query }) => {
-  const { id } = query;
-
-  return <h1>I am Detail Page with ID: {id}</h1>;
+  return axios
+    .post(`http://localhost:3000/graphql`, { query, variables })
+    .then(({ data: res }) => res.data)
+    .then((data) => data.portfolio);
 };
 
-PortfolioDetail.getInitialProps = ({ query }) => {
-  return { query };
+const PortfolioDetail = ({ portfolio }) => {
+  return (
+    <div className="portfolio-detail">
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-3">{portfolio.title}</h1>
+          <p className="lead">{portfolio.jobTitle}</p>
+          <p>
+            <a
+              className="btn btn-lg btn-success"
+              href={portfolio.companyWebsite}
+              role="button"
+            >
+              See Company
+            </a>
+          </p>
+        </div>
+
+        <div className="row marketing">
+          <div className="col-lg-6">
+            <h4 className="title">Location</h4>
+            <p className="text">{portfolio.location}</p>
+
+            <h4 className="title">Start Date</h4>
+            <p className="text">{portfolio.startDate}</p>
+          </div>
+
+          <div className="col-lg-6">
+            {/* TODO: days later... */}
+            <h4 className="title">Days</h4>
+            <p className="text">44</p>
+
+            <h4 className="title">End Date</h4>
+            <p className="text">{portfolio.endDate}</p>
+          </div>
+          <div className="col-md-12">
+            <hr />
+            <h4 className="title">Description</h4>
+            <p>{portfolio.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+PortfolioDetail.getInitialProps = async ({ query }) => {
+  const { id } = query;
+  const portfolio = await fetchPortfolioById(id);
+  return { portfolio };
 };
 
 export default PortfolioDetail;
